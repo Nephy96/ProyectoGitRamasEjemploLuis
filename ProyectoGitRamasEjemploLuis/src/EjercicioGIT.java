8000499668b08f41563843fa0e3e7abf4fbea8a0
alumno <alumno@DESKTOP-5FAD0MS>
2022-11-21 19:49:28 +0100

    Apartado 1
    Apartado 1

---
+++ ProyectoGitRamasEjemploLuis/src/EjercicioGIT.java
@@ -0,0 +1,10 @@
+//Este proyecto ha sido financiado por la comisión internacional de gente muy lista EU West ;)
+public class EjercicioGIT {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hola, mundo!");
+
+	}
+
+}
